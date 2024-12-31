c548d052ec7caaeddb24f048fb9b9b67e9e02651
Sameer Zafar <135354307+sameerzf@users.noreply.github.com>
2024-12-31 19:21:32 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors({credentials:true,origin:"http://localhost:5173"}));
 app.use('/api/routes',routes);
 
-connect('mongodb+srv://venomfire_2525:sameer123@cluster0.neida3j.mongodb.net/final-task').then(
+connect(URI).then(
     app.listen(5000,()=>{
     console.log("Server running")
 })).catch(error=>console.log(error))
